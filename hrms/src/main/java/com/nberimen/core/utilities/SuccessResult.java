a5edc29d2e568d6121e53bfdf54cce5fd0867c48
Necati Berimen <berimen66@gmail.com>
2021-05-27 01:32:23 +0300

    Result

---
+++ hrms/src/main/java/com/nberimen/core/utilities/SuccessResult.java
@@ -0,0 +1,12 @@
+package com.nberimen.core.utilities;
+
+public class SuccessResult extends Result {
+
+	public SuccessResult() {
+		super(true);
+	}
+
+	public SuccessResult(String message) {
+		super(true, message);
+	}
+}
