6b87892c3cde68808d182c6e851bb21e4b324d59
Necati Berimen <berimen66@gmail.com>
2021-05-27 01:29:11 +0300

    Swagger added

--- hrms/src/main/java/com/nberimen/HrmsApplication.java
+++ hrms/src/main/java/com/nberimen/HrmsApplication.java
@@ -7,7 +7,9 @@ import org.springframework.context.annotation.Bean;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2
 @SpringBootApplication
 public class HrmsApplication {
 
@@ -19,7 +21,7 @@ public class HrmsApplication {
 	public Docket api() {
 		return new Docket(DocumentationType.SWAGGER_2)
 				.select()
-				.apis(RequestHandlerSelectors.basePackage("com.nberimen.northwind"))
+				.apis(RequestHandlerSelectors.basePackage("com.nberimen"))
 				.build();
 	}
 
