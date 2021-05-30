52f09bc4fd6e11af1ef19f2d83f2d4a022805bfd
Necati Berimen <berimen66@gmail.com>
2021-05-30 03:08:47 +0300

    Update City.java

--- hrms/src/main/java/com/nberimen/city/City.java
+++ hrms/src/main/java/com/nberimen/city/City.java
@@ -2,13 +2,11 @@ package com.nberimen.city;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.Table;
 
 import lombok.Data;
 
 @Entity
 @Data
-@Table(name="cities")
 public class City {
 
 	@Id
