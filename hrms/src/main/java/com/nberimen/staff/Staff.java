df4ecb3c8f7e9a06dda6c0dcd6d2531bc8f92624
Necati Berimen <berimen66@gmail.com>
2021-05-30 03:09:05 +0300

    Update Staff.java

--- hrms/src/main/java/com/nberimen/staff/Staff.java
+++ hrms/src/main/java/com/nberimen/staff/Staff.java
@@ -1,25 +1,21 @@
 package com.nberimen.staff;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
-import javax.persistence.Table;
 
 import com.nberimen.user.User;
 
 import lombok.Data;
 
 @Entity
-@Table(name = "staff")
 @Data
 public class Staff {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "staff_id")
 	private int id;
 	
 	@OneToOne(optional = false)
