73a6172a46e0cbb190bf56519b9f553011241faf
Necati Berimen <berimen66@gmail.com>
2021-06-06 12:13:28 +0300

    Create Department.java

---
+++ hrms/src/main/java/com/nberimen/utilities/department/Department.java
@@ -0,0 +1,22 @@
+package com.nberimen.utilities.department;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.Data;
+
+@Data
+@Entity
+public class Department {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@JsonIgnore
+	private int id;
+	
+	private String departmentName;
+
+}
