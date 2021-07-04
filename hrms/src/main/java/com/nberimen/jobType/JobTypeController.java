fc36dcd92d10e343e85fa5c60af762f5cb416480
Necati Berimen <berimen66@gmail.com>
2021-07-04 20:12:12 +0300

    Create JobTypeController.java

---
+++ hrms/src/main/java/com/nberimen/jobType/JobTypeController.java
@@ -0,0 +1,52 @@
+package com.nberimen.jobType;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nberimen.utilities.result.ErrorDataResult;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api/jobTypes")
+public class JobTypeController {
+
+	@Autowired
+	private JobTypeService jobTypeService;
+	
+	@GetMapping("/getAll")
+	public ResponseEntity<?> getAll(){
+		return ResponseEntity.ok(this.jobTypeService.getAll());
+	}
+	@PostMapping("/add")
+	public ResponseEntity<?> addJobType(@Valid @RequestBody JobType jobType){
+		return ResponseEntity.ok(this.jobTypeService.addJobType(jobType));
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	public ErrorDataResult<Object> handleValidationException(MethodArgumentNotValidException exceptions){
+		Map<String, String> validationErrors= new HashMap<String, String>();
+		for(FieldError fieldError: exceptions.getBindingResult().getFieldErrors()) {
+			validationErrors.put(fieldError.getField(), fieldError.getDefaultMessage());
+		}
+		ErrorDataResult<Object> errors=new ErrorDataResult<Object>(validationErrors,"Doğrulama Hataları");
+		return errors;
+	}
+	
+}
