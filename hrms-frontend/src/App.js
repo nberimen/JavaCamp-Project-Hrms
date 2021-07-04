517bae5d157942e0f15206c955b6a382d15c1bc4
Necati Berimen <berimen66@gmail.com>
2021-07-04 20:26:07 +0300

    Update App.js

--- hrms-frontend/src/App.js
+++ hrms-frontend/src/App.js
@@ -1,17 +1,18 @@
 import './App.css';
 import Dashboard from './layouts/Dashboard';
 import 'semantic-ui-css/semantic.min.css'
-import { Container } from 'semantic-ui-react';
 import Navi from './layouts/Navi';
-function App() {
+import Footer from './layouts/Footer';
+
+const App = () => {
   return (
-    <div className="App">
-      <Navi />
-      <Container className="main">
+      <div className="App">
+        <Navi />
         <Dashboard />
-      </Container>
-    </div>
-  );
+        <Footer/>
+      </div>
+  )
 }
 
-export default App;
+export default App
+
