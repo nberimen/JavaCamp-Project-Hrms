ba350761d735a4883e1ea5ba61a97f7a2c02338a
Necati Berimen <berimen66@gmail.com>
2021-07-04 20:12:37 +0300

    Update JobPositionController.java

--- hrms/src/main/java/com/nberimen/jobposition/JobPositionController.java
+++ hrms/src/main/java/com/nberimen/jobposition/JobPositionController.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,6 +24,7 @@ import com.nberimen.utilities.result.ErrorDataResult;
 
 @RestController
 @RequestMapping("/api/positions")
+@CrossOrigin
 public class JobPositionController {
 
 	private JobPositionService jobPositionService;
