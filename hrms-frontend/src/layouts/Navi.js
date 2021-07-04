08622dda56c2c5391dd62dc0c5bf8f956411ae6d
Necati Berimen <berimen66@gmail.com>
2021-07-04 20:23:15 +0300

    Update Navi.js

--- hrms-frontend/src/layouts/Navi.js
+++ hrms-frontend/src/layouts/Navi.js
@@ -1,34 +1,39 @@
-import React, { useState } from 'react'
-import { Container, Menu } from 'semantic-ui-react'
-import SignedIn from './SignedIn'
-import SignedOut from './SignedOut'
-import { useHistory } from 'react-router'
+import React from 'react'
+import { NavLink } from "react-router-dom";
 export default function Navi() {
-
-    const [isAuthenticated, setIsAuthenticated] = useState(true)
-    const history = useHistory()
-
-    function handleSignOut() {
-        setIsAuthenticated(false)
-        history.push("/")
-    }
-    function handleSignIn() {
-        setIsAuthenticated(true)
-    }
+    
 
     return (
-        <div>
-
-            <Menu inverted >
-                <Container>
-                    <Menu.Item
-                    name="Home"
-                    />
-                    <Menu.Menu position='right'>
-                        {isAuthenticated ? <SignedIn signOut={handleSignOut} /> : <SignedOut signIn={handleSignIn} />}
-                    </Menu.Menu>
-                </Container>
-            </Menu>
+        <div >
+            
+            <div className="navbar fixed-top right navbar-expand-lg navbar-light bg-light" >
+                <div className="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
+                    <NavLink to="/home" className="navbar-brand">HRMS</NavLink>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                            <NavLink to="/"className="nav-link">İş Ara</NavLink>
+                            </li>
+                            <li className="nav-item dropdown show">
+                                <a className="nav-link dropdown-toggle " href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Dropdown</a>
+                                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <li><a className="dropdown-item" href="#">Action</a></li>
+                                    <li><a className="dropdown-item" href="#">Another action</a></li>
+                                    <li><hr className="dropdown-divider" /></li>
+                                    <li><a className="dropdown-item" href="#">Something else here</a></li>
+                                </ul>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                            </li>
+                        </ul>
+                        <form className="d-flex">
+                            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                            <button className="btn btn-outline-success" type="submit">Search</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
 
         </div>
     )
