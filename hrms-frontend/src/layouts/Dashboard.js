25b101ab5b1e95342d75d9112b39b6d8b0c75e23
Necati Berimen <berimen66@gmail.com>
2021-07-04 20:23:30 +0300

    Update Dashboard.js

--- hrms-frontend/src/layouts/Dashboard.js
+++ hrms-frontend/src/layouts/Dashboard.js
@@ -1,33 +1,20 @@
 import React from 'react'
-import { Grid } from 'semantic-ui-react'
-import CandidateList from '../pages/CandidateList';
-import JobPosition from './JobPosition'
-import { Route } from 'react-router';
-import CandidateDetail from '../pages/CandidateDetail';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import JobAdvertPage from '../pages/JobAdvertPage'
+import HomePage from '../pages/HomePage';
+import SignupPage from '../pages/SignupPage';
 
 export default function Dashboard() {
     return (
-        <div>
-            <Grid>
-                <Grid.Row>
-
-                    <Grid.Column width={4}>
-                        <JobPosition/>
-                    </Grid.Column>
-
-                    <Grid.Column width={12}>
-                    <Route exact path="/" component={CandidateList}/>
-                    <Route exact path="/candidates" component={CandidateList}/>
-                    <Route path="/candidates/:id" component={CandidateDetail}/>
-
-                    </Grid.Column>
-
-                </Grid.Row>
-            </Grid>
-
-
-        </div>
-
-
+        <Router>
+            <div>
+                <Switch>
+                    <Route exact path="/" component={HomePage} />
+                    <Route exact path="/home" component={HomePage} />
+                    <Route exact path="/advert-job" component={JobAdvertPage} />
+                    <Route exact path="/signup" component={SignupPage} />
+                </Switch>
+            </div>
+        </Router>
     );
 }
