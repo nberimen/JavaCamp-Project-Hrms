0a2760ba984a5c54f5f8465ce630ac375a990712
Necati Berimen <berimen66@gmail.com>
2021-07-04 20:22:44 +0300

    Create HomePage.js

---
+++ hrms-frontend/src/pages/HomePage.js
@@ -0,0 +1,15 @@
+import React from "react";
+//import hrms from '../images/hrms.png'
+//import { FaBriefcase, FaRegAddressCard, FaUserCheck } from 'react-icons/fa';
+
+export default function HomePage() {
+    return (
+        <div style={{minHeight:"50vh"}}>
+          
+
+
+
+          
+        </div>
+    );
+}
