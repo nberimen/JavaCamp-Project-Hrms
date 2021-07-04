606e3a936cccd04f93cf7c185b2a0e6ca082eeab
Necati Berimen <berimen66@gmail.com>
2021-07-04 20:17:12 +0300

    Update CityController.java

--- hrms/src/main/java/com/nberimen/city/CityController.java
+++ hrms/src/main/java/com/nberimen/city/CityController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,6 +22,7 @@ import com.nberimen.utilities.result.ErrorDataResult;
 
 @RestController
 @RequestMapping("/api/cities")
+@CrossOrigin
 public class CityController {
 
 	private CityService cityService;
